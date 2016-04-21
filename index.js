9f5465536f9290022fef3a684b7255a6db59059d
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2016-04-21 10:58:27 +0200

    fixed bug with id again

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function compile(id, str) {
   // the id will be the filename and path relative to the require()ing module
   return 'function(data) {'+
     '  var template = twig({'+
-    '    id: ' + id + ','+
+    '    id: \'' + id + '\','+
     '    data:' + tokens + ','+
     '    precompiled: true,'+
     '    allowInlineIncludes: true'+
